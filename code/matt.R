815deee3dff7ce42d0df12f5d4d702bc28a92826
CY-Chan <j-chan@kellogg.northwestern.edu>
2014-10-18 20:54:26 -0500

    Simplified probability calculations. Also checked that Matt's calculations for method 2 matches with Johnny's calculations.
    Signed-off-by: CY-Chan <j-chan@kellogg.northwestern.edu>

--- code/matt.R
+++ code/matt.R
@@ -8,7 +8,6 @@ lambda<-readRDS(paste0(varSave, 'lambda_values.rds'))
 # Simulate 10,000 choices corresponding to V_ik and lambda_k values: 
 N <- 10^5
 epsilon <- rgev(n = nrow(lambda)*N, xi = 0, mu = 0, beta = 1)
-
 eta <- rgev(n = length(unique(val$choice))*N, xi = 0, mu = 0, beta = 1)
 
 # 1 under the traditional specification where epsilon_k resolves before eta_i
@@ -41,55 +40,32 @@ item_choice1 <- data.frame(
 # 2 under the single-shot specification where epsilon_k and eta_i both resolve up front  
 a <- matrix(eta,nrow = 3,byrow = TRUE)
 U2 <- apply(
+  temp<-
   val$val + 
   matrix(rep(epsilon,3),nrow = 9, byrow = TRUE) + 
   rep(lambda$lambda,3) * 
-  a[rep(seq(nrow(a)),each = 3),],2,which.max
+  a[rep(seq(nrow(a)),each = 3),],
+  2,
+  which.max
 )
 item_choice2 <- data.frame(
   "bucket" = val$bucket[U2],
   "choice" = val$choice[U2]
 )
 
-
 # Estimate choice probabilities and calculate covariance matrix using bootstrap
-
-# 1  under the traditional specification where epsilon_k resolves before eta_i
-Prob1 <- data.frame(
-  ProbA1 = with(item_choice1,sum(bucket == 1 & choice == "A")/nrow(item_choice1)),
-  ProbB1 = with(item_choice1,sum(bucket == 1 & choice == "B")/nrow(item_choice1)),
-  ProbC1 = with(item_choice1,sum(bucket == 1 & choice == "C")/nrow(item_choice1)),
-  ProbA2 = with(item_choice1,sum(bucket == 2 & choice == "A")/nrow(item_choice1)),
-  ProbB2 = with(item_choice1,sum(bucket == 2 & choice == "B")/nrow(item_choice1)),
-  ProbC2 = with(item_choice1,sum(bucket == 2 & choice == "C")/nrow(item_choice1)),
-  ProbA3 = with(item_choice1,sum(bucket == 3 & choice == "A")/nrow(item_choice1)),
-  ProbB3 = with(item_choice1,sum(bucket == 3 & choice == "B")/nrow(item_choice1)),
-  ProbC3 = with(item_choice1,sum(bucket == 3 & choice == "C")/nrow(item_choice1))
-)
-                    
-
-
+# 1 under the traditional specification where epsilon_k resolves before eta_i
 # 2 under the single-shot specification where epsilon_k and eta_i both resolve up front
-Prob2 <- data.frame(
-  ProbA1 = with(item_choice2,sum(bucket == 1 & choice == "A")/nrow(item_choice2)),
-  ProbB1 = with(item_choice2,sum(bucket == 1 & choice == "B")/nrow(item_choice2)),
-  ProbC1 = with(item_choice2,sum(bucket == 1 & choice == "C")/nrow(item_choice2)),
-  ProbA2 = with(item_choice2,sum(bucket == 2 & choice == "A")/nrow(item_choice2)),
-  ProbB2 = with(item_choice2,sum(bucket == 2 & choice == "B")/nrow(item_choice2)),
-  ProbC2 = with(item_choice2,sum(bucket == 2 & choice == "C")/nrow(item_choice2)),
-  ProbA3 = with(item_choice2,sum(bucket == 3 & choice == "A")/nrow(item_choice2)),
-  ProbB3 = with(item_choice2,sum(bucket == 3 & choice == "B")/nrow(item_choice2)),
-  ProbC3 = with(item_choice2,sum(bucket == 3 & choice == "C")/nrow(item_choice2))
-)
 
-
-
-
-
-
-
-
-
-
-
+k_i<- array(val[,1:2])
+Prob1<- 
+  mdply(k_i, 
+        function(k,i) with(item_choice1,sum(bucket == k & choice == i)/nrow(item_choice1))
+        )
+colnames(Prob1)<- c("bucket", "choice", "prob")
+Prob2<- 
+  mdply(k_i, 
+        function(k,i) with(item_choice2,sum(bucket == k & choice == i)/nrow(item_choice2))
+  )
+colnames(Prob1)<- c("bucket", "choice", "prob")
 
